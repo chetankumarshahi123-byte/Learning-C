27703190c1ba335b978ce268f3aad33c34e55996
Chetan Shahi <chetankumarshahi123@gmail.com>
2026-07-13 14:03:34 +0545

    Create subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sub;
+printf("Enter a: ");
+scanf("%d",&a);
+printf("Enter b: ");
+scanf("%d",&b);
+sub = a-b;
+printf("Subtraction of two numbers is %d",sub);
+return 0;
+}
