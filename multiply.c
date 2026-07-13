710f2e6b45841e9dce967f265a0fce828914e0ce
Chetan Shahi <chetankumarshahi123@gmail.com>
2026-07-13 14:09:17 +0545

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,multi;
+printf("Enter a: ");
+scanf("%d",&a);
+printf("Enter b: ");
+scanf("%d",&b);
+multi = a*b;
+printf("Multiple of three numbers is %d",multi);
+return 0;
+}
