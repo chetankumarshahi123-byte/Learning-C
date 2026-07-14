c2c4a9dde7e870d916c04134bf893d15d2f08afa
Chetan Shahi <chetankumarshahi123@gmail.com>
2026-07-14 13:58:37 +0545

    Create volume.c

---
+++ volume.c
@@ -0,0 +1,14 @@
+// WAP to find the volume of cube in C.
+
+#include<stdio.h>
+int main()
+{
+float side, volume;
+printf("Enter side: ");
+scanf("%f",&side);
+volume = side*side*side;
+printf("volume = %.2f m³", volume);
+return 0;
+}
+
+
